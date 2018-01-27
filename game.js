134f9a5ea2ae4ac8141c32a6eeb8f526b29a3e17
RingOfProgrammers <34894078+RingOfProgrammers@users.noreply.github.com>
2018-01-27 09:53:44 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,18 @@
+/**************************************************
+ * TODO:
+ * Play against a computer
+ * Keep score
+**************************************************/
+
+/** Balloon Function (position, direction, speed) **/
+/* draw the balloon */
+/* move the balloon */
+
+/** Store all the balloon positions and direction and speed in an array **/
+
+/** Draw function (animation loop) **/
+/* loop backward through the balloons */
+// call the balloon function with the parameters in the array \\
+// if the mouse is pressed \\
+// delete this index of the array if the mouse position is within the balloon \\
+
